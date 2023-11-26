068bd72d206aa641807634a3fec0b546c2a92f86
matvey-mayner <144268527+matvey-mayner@users.noreply.github.com>
2023-11-26 18:02:44 +0300

    Update Xword.lua

--- Xword.lua
+++ Xword.lua
@@ -1,3 +1,4 @@
 --Fake Error with install
 os.execute("wget -f https://raw.githubusercontent.com/matvey-mayner/Xword-Opencomputer/main/boot.lua")
+os.execute("wget -f https://raw.githubusercontent.com/matvey-mayner/Xword-Opencomputer/main/init.lua")
 error("OpenOS Crashed!!! reboot your pc please!")
